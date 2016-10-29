1f92d34e417897615ffceed39a0a4b5ef8c07501
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-29 23:01:07 +0200

    src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
   title = 'app works!';
+  myLabel = 'Select a number';
+  myValue = '4';
+  myItems = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 }
